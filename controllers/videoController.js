4f661179ca22b661ee83801c1ed38feee9775665
Dias-Gabriel-Dev <gabriel.henrique.dias@outlook.com>
2025-04-26 12:13:17 -0300

    refatoração do controlador para exportação de classes e alteração numa regra de validação que permitia mudar a URL

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,39 +1,57 @@
 import Video from "../models/Video.js";
 
-export const criarVideo = async (req, res) => {
-    try {
-      const novoVideo = await Video.create(req.body);
-      res.status(201).json('Vídeo postado com sucesso',novoVideo);
-    } catch (erro) {
-      res.status(400).json({ erro: erro.message });
-    }
-};
+class VideoController {
 
-export const ListaTodosOsVideos = async (req, res) => {
-    try {
-      const videos = await Video.find();
-      res.json(videos);
-    } catch (erro) {
-      res.status(500).json({ erro: 'Erro interno do servidor'});
-    }
-};
+  static async criarVideo(req, res) {
+      try {
+        const novoVideo = await Video.create(req.body);
+        res.status(201).json({message: 'Vídeo postado com sucesso', video: novoVideo});
+      } catch (erro) {
+        res.status(400).json({ erro: erro.message });
+      }
+  }
 
-export const atualizarVideo = async (req, res) => {
-    try {
-       const video = await Video.findByIdAndUpdate(req.params.id, req.body,
-        { new: true }
-       );
-       video ? res.json('Atualizações feitas com sucesso!' ,video) : res.status(404).json({ erro: 'Vídeo não encontrado.'});
-    } catch (erro) {
-        res.status(400).json({ erro: erro.message })
-    }
-};
+  static async ListaTodosOsVideos(req, res) {
+      try {
+        const videos = await Video.find();
+        res.json(videos);
+      } catch (erro) {
+        res.status(500).json({ erro: 'Erro interno do servidor'});
+      }
+  }
 
-export const deletarVideo = async (req, res) => {
-    try {
-       const video = await Video.findByIdAndDelete(req.params.id);
-       video ? res.status(204).json('Vídeo removido com sucesso!') : res.status(404).json({ erro: 'Vídeo não encontrado'});
-    } catch (error) {
-       res.status(400).json({ erro: 'Não foi possível remover o vídeo' });
+  static async listarVideoPorId(req, res) {
+      try {
+        const id = req.params.id;
+        const videoEncontrado = await Video.findById(id);
+        res.status(201).json(videoEncontrado);
+      } catch (error) {
+        res.status(500).json({ message: 'Vídeo não encontrado'})
     }
-};
+  }
+
+  static async atualizarVideo(req, res) {
+      try {
+        const videoAtualizado = await Video.findByIdAndUpdate(req.params.id, req.body,
+          { new: true, runValidators: true }
+        );
+        if (!videoAtualizado) {
+          return res.status(404).json({ message: 'Vídeo não encontrado'});
+        }
+        res.status(201).json({ message: 'Vídeo atualizado com sucesso', video: videoAtualizado})
+      } catch (erro) {
+          res.status(400).json({ erro: erro.message })
+      }
+  }
+
+  static async deletarVideo(req, res) {
+      try {
+        const videoDeletado = await Video.findByIdAndDelete(req.params.id);
+        res.status(201).json({ message: 'Vídeo removido com sucesso!', video: videoDeletado })
+      } catch (error) {
+        res.status(400).json({ erro: 'Não foi possível remover o vídeo' });
+      }
+  };
+}
+
+export default VideoController;
